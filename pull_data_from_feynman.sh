0de1c338b39f64c0645147e784f7d2b9d0af04d4
Tapas Sahoo <tapascuchem@gmail.com>
2022-04-22 08:09:56 +0530

    update

--- pull_data_from_feynman.sh
+++ pull_data_from_feynman.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/ground-state-*-vs-Rpt* /Users/tsahoo/dev-project/outputs/exact-computation/
-rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/ground-state-theta-* /Users/tsahoo/dev-project/outputs/exact-computation/
-rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/ground-state-order-parameters-* /Users/tsahoo/dev-project/outputs/exact-computation/ 
-rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/eigen-values* /Users/tsahoo/dev-project/outputs/exact-computation/
+rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/ground-state-*-vs-Rpt* /Users/tsahoo/academic-project/outputs/exact-computation/
+rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/ground-state-theta-* /Users/tsahoo/academic-project/outputs/exact-computation/
+rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/ground-state-order-parameters-* /Users/tsahoo/academic-project/outputs/exact-computation/ 
+rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/eigen-values* /Users/tsahoo/academic-project/outputs/exact-computation/
--- pull_data_from_graham.sh
+++ pull_data_from_graham.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rsync -avze ssh graham:/home/tapas/ResultsOfENT/* /Users/tsahoo/dev-project/outputs/results-of-ent/
+rsync -avze ssh graham:/home/tapas/ResultsOfENT/* /Users/tsahoo/academic-project/outputs/results-of-ent/
 #rsync -avze ssh graham:/home/tapas/ResultsOfPIGS/* /Users/tsahoo/ResultsOfPIGS/ --exclude 'PIGS-qTIP4P-RotDOFs-Rpt[789].[02468]Angstrom*-beta0.2Kinv*System2-p-H2O*' --exclude 'PIGS-qTIP4P-RotDOFs-Rpt10.0Angstrom*-beta0.2Kinv*System2-p-H2O*' 
-rsync -avze ssh graham:/home/tapas/ResultsOfPIGS/* /Users/tsahoo/dev-project/outputs/results-of-pigs/
-rsync -avze ssh graham:/home/tapas/ResultsOfPIMC/* /Users/tsahoo/dev-project/outputs/results-of-pimc/
+rsync -avze ssh graham:/home/tapas/ResultsOfPIGS/* --exclude 'core.*' /Users/tsahoo/academic-project/outputs/results-of-pigs/
+rsync -avze ssh graham:/home/tapas/ResultsOfPIMC/* /Users/tsahoo/academic-project/outputs/results-of-pimc/
 #rsync -avze ssh graham:/scratch/tapas/linear-rotors/*gFactor[12468].0*System2HF*_ENSMBL61 /Users/tsahoo/linear-rotors/
 #rsync -avze ssh graham:/scratch/tapas/linear-rotors/*gFactor16.0*System2HF*_ENSMBL[456][1] /Users/tsahoo/linear-rotors/
 #rsync -avze ssh graham:/scratch/tapas/linear-rotors/*gFactor[012].[05]*System8HF*_ENSMBL-WOR-51 /Users/tsahoo/linear-rotors/
