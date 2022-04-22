b2b7fb70f775febec88c8b60b6ccfd02328b6ad5
Tapas Sahoo <tapascuchem@gmail.com>
2022-04-22 09:57:00 +0530

    update +

--- push_system_scripts.sh
+++ push_system_scripts.sh
@@ -9,13 +9,13 @@ cp /usr/local/bin/push_system_scripts.sh /Users/tsahoo/academic-project/system-s
 #
 cp ~/.ssh/config /Users/tsahoo/academic-project/system-scripts/ssh-config
 cp ~/.vimrc /Users/tsahoo/academic-project/system-scripts/vimrc-script
-cp ~/.bashrc /Users/tsahoo/academic-project/system-scripts/bashrc
+#cp ~/.bashrc /Users/tsahoo/academic-project/system-scripts/bashrc
 cp ~/.bash_profile /Users/tsahoo/academic-project/system-scripts/bash_profile
 cp ~/.zshrc /Users/tsahoo/academic-project/system-scripts/zshrc
 cp ~/.zprofile /Users/tsahoo/academic-project/system-scripts/zprofile
 cp ~/.gitignore /Users/tsahoo/academic-project/system-scripts/gitignore
 
 cd /Users/tsahoo/academic-project/system-scripts/
-git remote set-url origin https://github.com/tapassahoo/system-scripts.git
+#git remote set-url origin https://github.com/tapassahoo/system-scripts.git
 
-git add . && git commit -m 'update' && git push
+git add . && git commit -m "update "+$(day) && git push
