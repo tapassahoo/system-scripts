5fe56aaf2a1dab125bef7fa2bc2ffdb39e9310b8
Tapas Sahoo <tapascuchem@gmail.com>
2022-07-22 21:46:08 +0530

    Updated on 22-07-2022

--- pull_data_from_plot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rsync -avze ssh graham:/home/tapas/ResultsOfPIGS/*.pdf --exclude="core.*" /Users/tsahoo/academic-project/outputs/results-of-pigs/
-rsync -avze ssh graham:/home/tapas/ResultsOfPIGS/for-vmd/*.xyz --exclude="core.*" /Users/tsahoo/academic-project/outputs/results-of-pigs/for-vmd/
-rsync -avze ssh graham:/home/tapas/ResultsOfPIGS/for-vmd/*.mpg --exclude="core.*" /Users/tsahoo/academic-project/outputs/results-of-pigs/for-vmd/
