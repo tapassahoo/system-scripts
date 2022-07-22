24685c2e28255a8baad77809c831e81d28dcc88e
Tapas Sahoo <tapascuchem@gmail.com>
2022-07-22 21:51:01 +0530

    Updated on 22-07-2022

--- push-system-scripts.sh
+++ push-system-scripts.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-cp /usr/local/bin/pull-data-from-graham.sh ~/academic-project/system-scripts/ 
-cp /usr/local/bin/pull-data-from-cedar.sh ~/academic-project/system-scripts/
-cp /usr/local/bin/pull-data-from-feynman.sh ~/academic-project/system-scripts/ 
-cp /usr/local/bin/pull-data-from-plot.sh ~/academic-project/system-scripts/ 
-cp /usr/local/bin/push-article-uw.sh ~/academic-project/system-scripts/
-cp /usr/local/bin/push-system-scripts.sh ~/academic-project/system-scripts/     
+rsync -r /usr/local/bin/pull-data-from-graham.sh ~/academic-project/system-scripts/ 
+rsync -r /usr/local/bin/pull-data-from-cedar.sh ~/academic-project/system-scripts/
+rsync -r /usr/local/bin/pull-data-from-feynman.sh ~/academic-project/system-scripts/ 
+rsync -r /usr/local/bin/pull-data-from-plot.sh ~/academic-project/system-scripts/ 
+rsync -r /usr/local/bin/push-article-uw.sh ~/academic-project/system-scripts/
+rsync -r /usr/local/bin/push-system-scripts.sh ~/academic-project/system-scripts/     
 #
-cp ~/.ssh/config ~/academic-project/system-scripts/ssh-config-macos
-cp ~/.vimrc ~/academic-project/system-scripts/vimrc-script-macos
-cp ~/.zshrc /Users/tsahoo/academic-project/system-scripts/zshrc-macos
-cp ~/.zprofile /Users/tsahoo/academic-project/system-scripts/zprofile-macos
-cp ~/.gitignore /Users/tsahoo/academic-project/system-scripts/gitignore-macos
+rsync -r ~/.ssh/config ~/academic-project/system-scripts/ssh-config-macos
+rsync -r ~/.vimrc ~/academic-project/system-scripts/vimrc-script-macos
+rsync -r ~/.zshrc /Users/tsahoo/academic-project/system-scripts/zshrc-macos
+rsync -r ~/.zprofile /Users/tsahoo/academic-project/system-scripts/zprofile-macos
+rsync -r ~/.gitignore /Users/tsahoo/academic-project/system-scripts/gitignore-macos
 
 cd /Users/tsahoo/academic-project/system-scripts/
 acp
