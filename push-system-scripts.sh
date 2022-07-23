06cffd471fb387423562082d44625328e87b0f24
Tapas Sahoo <tapascuchem@gmail.com>
2022-07-23 22:39:48 +0530

    Updated on 23-07-2022

--- push-system-scripts.sh
+++ push-system-scripts.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/zsh
 rsync -r /usr/local/bin/pull-data-from-graham.sh ~/academic-project/system-scripts/ 
 rsync -r /usr/local/bin/pull-data-from-cedar.sh ~/academic-project/system-scripts/
 rsync -r /usr/local/bin/pull-data-from-feynman.sh ~/academic-project/system-scripts/ 
