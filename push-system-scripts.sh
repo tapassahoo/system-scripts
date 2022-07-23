ca54c2938a9728a0384296c4f08dd8e654e74527
Tapas Sahoo <tapascuchem@gmail.com>
2022-07-23 22:50:57 +0530

    Updated on 23-07-2022

--- push-system-scripts.sh
+++ push-system-scripts.sh
@@ -12,5 +12,4 @@ rsync -r ~/.zshrc ~/academic-project/system-scripts/zshrc-macos
 rsync -r ~/.zprofile ~/academic-project/system-scripts/zprofile-macos
 rsync -r ~/.gitignore ~/academic-project/system-scripts/gitignore-macos
 
-source ~/.zshrc
 cd ~/academic-project/system-scripts/; acp
