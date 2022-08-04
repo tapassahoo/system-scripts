42460d2e08d6425dbe2c09cbf452c30e38633484
“Tapas <tapascuchem@gmail.com>
2022-08-04 15:21:34 +0530

    Updated on 04-08-2022

--- push-system-scripts-ubuntu.sh
+++ push-system-scripts-ubuntu.sh
@@ -6,6 +6,9 @@ set -xv
 #===============================================================
 cp ~/.bashrc $HOME/academic-project/system-scripts/bashrc-ubuntu
 cp ~/.vimrc  $HOME/academic-project/system-scripts/vimrc-ubuntu
+cp ~/.gitignore  $HOME/academic-project/system-scripts/gitignore-ubuntu
+cp ~/.ssh/config $HOME/academic-project/system-scripts/ssh-config-ubuntu
+#
 cp -r /usr/local/bin/push-system-scripts.sh $HOME/academic-project/system-scripts/push-system-scripts-ubuntu.sh
 cp -r /usr/local/bin/pull-data-from-graham.sh $HOME/academic-project/system-scripts/pull-data-from-graham-ubuntu.sh
 
