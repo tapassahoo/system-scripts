eb41014bc8c261e422eaab05864a6bfe3b1d7980
“Tapas <tapascuchem@gmail.com>
2022-08-08 12:09:44 +0530

    Linux: Updated on 08-08-2022

---
+++ push-system-scripts-Linux.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "================================================================"
+echo ""
+echo "CRON:" `date`
+set -xv
+#===============================================================
+cp ~/.bashrc ${HOME}/academic-project/system-scripts/bashrc-$(uname)
+cp ~/.vimrc  ${HOME}/academic-project/system-scripts/vimrc-$(uname)
+cp ~/.gitignore  ${HOME}/academic-project/system-scripts/gitignore-$(uname)
+cp ~/.ssh/config ${HOME}/academic-project/system-scripts/ssh-config-$(uname)
+#
+cp -r /usr/local/bin/push-system-scripts.sh ${HOME}/academic-project/system-scripts/push-system-scripts-$(uname).sh
+cp -r /usr/local/bin/pull-data-from-graham.sh ${HOME}/academic-project/system-scripts/pull-data-from-graham-$(uname).sh
+
+cd ${HOME}/academic-project/system-scripts/
+git pull && git add . && git commit -m "$(uname): Updated on $(date +"%d-%m-%Y")" && git push https://github.com/tapassahoo/System-Scripts.git 
