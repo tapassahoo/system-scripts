aafdefe236ae32724ea6ddcb44fd58866d2c7d95
“Tapas <tapascuchem@gmail.com>
2022-08-08 16:18:17 +0530

    Linux: Updated on 08-08-2022

--- pull-data-from-feynman.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/ground-state-*-vs-Rpt* /Users/tsahoo/academic-project/outputs/exact-computation/
-rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/ground-state-theta-* /Users/tsahoo/academic-project/outputs/exact-computation/
-rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/ground-state-order-parameters-* /Users/tsahoo/academic-project/outputs/exact-computation/ 
-rsync -avzhe ssh feynman:/home/tapas/CodesForEigenValues/nonlinear-rotors/exact-energies-of-H2O/eigen-values* /Users/tsahoo/academic-project/outputs/exact-computation/
