7e9b8c65c2127793b37bea709aeb49daa18feb7a
Tapas Sahoo <tapascuchem@gmail.com>
2022-09-03 14:00:02 +0530

    Darwin: Updated on 03-09-2022

--- pull-data-from-graham-Darwin.sh
+++ pull-data-from-graham-Darwin.sh
@@ -4,7 +4,7 @@ echo ""
 echo ""
 echo "CRON:" `date`
 set -xv
-#rsync -avze ssh graham:/home/tapas/ResultsOfENT/* /Users/tsahoo/academic-project/outputs/results-of-ent/
+rsync -avze ssh graham:/home/tapas/final-pigs-outputs-for-plotting/* ${HOME}/academic-project/outputs/final-pigs-outputs-for-plotting/
 #rsync -avze ssh graham:/home/tapas/ResultsOfPIGS/* /Users/tsahoo/ResultsOfPIGS/ --exclude 'PIGS-qTIP4P-RotDOFs-Rpt[789].[02468]Angstrom*-beta0.2Kinv*System2-p-H2O*' --exclude 'PIGS-qTIP4P-RotDOFs-Rpt10.0Angstrom*-beta0.2Kinv*System2-p-H2O*' 
 #rsync -avze ssh graham:/home/tapas/ResultsOfPIGS/* --exclude 'core.*' /Users/tsahoo/academic-project/outputs/results-of-pigs/
 #rsync -avze ssh graham:/home/tapas/ResultsOfPIMC/* /Users/tsahoo/academic-project/outputs/results-of-pimc/
@@ -16,6 +16,6 @@ set -xv
 #rsync -avze ssh graham:/scratch/tapas/nonlinear-rotors/PIGS-qTIP4P-RotDOFs-Rpt3.[0-9]Angstrom*-beta0.1Kinv*System2-p-H2O*beads* /Users/tsahoo/nonlinear-rotors/
 #rsync -avze ssh graham:/scratch/tapas/nonlinear-rotors/PIGS-qTIP4P-RotDOFs-Rpt[23].[0-9]Angstrom*-beta0.1Kinv*System11-p-H2O*beads101 /Users/tsahoo/nonlinear-rotors/
 #rsync -avze ssh graham:/scratch/tapas/nonlinear-rotors/PIGS-qTIP4P-RotDOFs-Rpt[456].[05]Angstrom*-beta0.1Kinv*System11-p-H2O*beads101 /Users/tsahoo/nonlinear-rotors/
-rsync -avze ssh graham:/scratch/tapas/rot-dens-asymmetric-top-plot/rot-dens-matrix-of-para-H2O-Temp5.000000Kelvin-Beads24/rho.den018 --exclude 'core.*' ${HOME}/academic-project/outputs/rotational-propagator-for-water/rho.den018-para
-rsync -avze ssh graham:/scratch/tapas/rot-dens-asymmetric-top-plot/rot-dens-matrix-of-ortho-H2O-Temp5.000000Kelvin-Beads24/rho.den018 --exclude 'core.*' ${HOME}/academic-project/outputs/rotational-propagator-for-water/rho.den018-ortho
-rsync -avze ssh graham:/scratch/tapas/rot-dens-asymmetric-top-plot/rot-dens-matrix-of-spin-less-H2O-Temp5.000000Kelvin-Beads24/rho.den018 --exclude 'core.*' ${HOME}/academic-project/outputs/rotational-propagator-for-water/rho.den018-spinless
+#rsync -avze ssh graham:/scratch/tapas/rot-dens-asymmetric-top-plot/rot-dens-matrix-of-para-H2O-Temp5.000000Kelvin-Beads24/rho.den018 --exclude 'core.*' ${HOME}/academic-project/outputs/rotational-propagator-for-water/rho.den018-para
+#rsync -avze ssh graham:/scratch/tapas/rot-dens-asymmetric-top-plot/rot-dens-matrix-of-ortho-H2O-Temp5.000000Kelvin-Beads24/rho.den018 --exclude 'core.*' ${HOME}/academic-project/outputs/rotational-propagator-for-water/rho.den018-ortho
+#rsync -avze ssh graham:/scratch/tapas/rot-dens-asymmetric-top-plot/rot-dens-matrix-of-spin-less-H2O-Temp5.000000Kelvin-Beads24/rho.den018 --exclude 'core.*' ${HOME}/academic-project/outputs/rotational-propagator-for-water/rho.den018-spinless
