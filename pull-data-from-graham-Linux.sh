f8620bec2b07c4d649d58b0de369f57d864fde43
Tapas Sahoo <tapascuchem@gmail.com>
2022-09-22 12:00:01 +0530

    Linux: Updated on 22-09-2022

--- pull-data-from-graham-Linux.sh
+++ pull-data-from-graham-Linux.sh
@@ -3,16 +3,11 @@ echo ""
 echo ""
 echo "CRON:" `date`
 set -xv
-#rsync -avze ssh graham:/home/tapas/ResultsOfENT/* /Users/tsahoo/academic-project/outputs/results-of-ent/
-#rsync -avze ssh graham:/home/tapas/ResultsOfPIGS/* /Users/tsahoo/ResultsOfPIGS/ --exclude 'PIGS-qTIP4P-RotDOFs-Rpt[789].[02468]Angstrom*-beta0.2Kinv*System2-p-H2O*' --exclude 'PIGS-qTIP4P-RotDOFs-Rpt10.0Angstrom*-beta0.2Kinv*System2-p-H2O*' 
-#rsync -avze ssh graham:/home/tapas/ResultsOfPIMC/* /Users/tsahoo/academic-project/outputs/results-of-pimc/
-#rsync -avze ssh graham:/scratch/tapas/linear-rotors/*gFactor[12468].0*System2HF*_ENSMBL61 /Users/tsahoo/linear-rotors/
-#rsync -avze ssh graham:/scratch/tapas/linear-rotors/*gFactor16.0*System2HF*_ENSMBL[456][1] /Users/tsahoo/linear-rotors/
-#rsync -avze ssh graham:/scratch/tapas/linear-rotors/*gFactor[012].[05]*System8HF*_ENSMBL-WOR-51 /Users/tsahoo/linear-rotors/
-#rsync -avze ssh graham:/scratch/tapas/linear-rotors/*gFactor[012].[05]*System8HF*_ENSMBL-WOR-61 /Users/tsahoo/linear-rotors/
-#rsync -avze ssh graham:/scratch/tapas/linear-rotors/*gFactor[012].[05]*System8HF*_ENSMBL-WOR-41 /Users/tsahoo/linear-rotors/
-#rsync -avze ssh graham:/scratch/tapas/nonlinear-rotors/PIGS-qTIP4P-RotDOFs-Rpt3.[0-9]Angstrom*-beta0.1Kinv*System2-p-H2O*beads* /Users/tsahoo/nonlinear-rotors/
-#rsync -avze ssh graham:/scratch/tapas/nonlinear-rotors/PIGS-qTIP4P-RotDOFs-Rpt[23].[0-9]Angstrom*-beta0.1Kinv*System11-p-H2O*beads101 /Users/tsahoo/nonlinear-rotors/
-#rsync -avze ssh graham:/scratch/tapas/nonlinear-rotors/PIGS-qTIP4P-RotDOFs-Rpt[456].[05]Angstrom*-beta0.1Kinv*System11-p-H2O*beads101 /Users/tsahoo/nonlinear-rotors/
 rsync -avze ssh graham:/home/tapas/final-pigs-outputs-for-plotting/* --exclude 'core.*' ${HOME}/academic-project/outputs/final-pigs-outputs-for-plotting/
 rsync -avze ssh graham:/home/tapas/final-dmrg-outputs-for-plotting/* --exclude 'core.*' ${HOME}/academic-project/outputs/final-dmrg-outputs-for-plotting/
+#
+#************************************ Some usefull examples *********************************************************#
+#
+#rsync -avze ssh graham:/home/tapas/ResultsOfPIGS/* /Users/tsahoo/ResultsOfPIGS/ --exclude 'PIGS-qTIP4P-RotDOFs-Rpt[789].[02468]Angstrom*-beta0.2Kinv*System2-p-H2O*' --exclude 'PIGS-qTIP4P-RotDOFs-Rpt10.0Angstrom*-beta0.2Kinv*System2-p-H2O*' 
+#rsync -avze ssh graham:/scratch/tapas/linear-rotors/*gFactor16.0*System2HF*_ENSMBL[456][1] /Users/tsahoo/linear-rotors/
+#rsync -avze ssh graham:/scratch/tapas/nonlinear-rotors/PIGS-qTIP4P-RotDOFs-Rpt[23].[0-9]Angstrom*-beta0.1Kinv*System11-p-H2O*beads101 /Users/tsahoo/nonlinear-rotors/
