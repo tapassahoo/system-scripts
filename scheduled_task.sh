3a579c4876389c51de9f63f9d407cca062aeb6f3
Tapas Sahoo <tapascuchem@gmail.com>
2022-11-12 11:36:20 +0530

    Darwin: Updated on 12-11-2022

--- scheduled_task.sh
+++ scheduled_task.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo -e "\n"
 date 
-salloc --time=0-00:30 --ntasks=1 --account=rrg-pnroy_cpu
-#cd /home/tapas/MoRiBS-PIGS/examples/scripts/
-#cp generic_execution_moribs_driver_linear_molecule.py temp_execution_moribs_driver_linear_molecule_n5_beta01_nskip15000.py
-#sed -i 's/\<NUMB_MOLECULE\>/5/' temp_execution_moribs_driver_linear_molecule_n5_beta01_nskip15000.py 
-#sed -i 's/\<PARAMETER_VALUE\>/0.1/' temp_execution_moribs_driver_linear_molecule_n5_beta01_nskip15000.py
-#sed -i 's/\<NUMB_SKIP\>/15000/' temp_execution_moribs_driver_linear_molecule_n5_beta01_nskip15000.py
-#python temp_execution_moribs_driver_linear_molecule_n5_beta01_nskip15000.py
-pwd
-exit
-pwd
+var_job_type=$1
+var_molecule=$2
+var_parameter=$3
+var_preskip=$4
+pwd 
+echo "job_type       = $var_job_type"
+echo "numb_molecule  = $var_molecule"
+echo "numb_parameter = $var_parameter"
+echo "numb_preskip   = $var_preskip"
+cd /home/tapas/MoRiBS-PIGS/examples/scripts/
+cp generic_execution_moribs_driver_linear_molecule.py temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py
+sed -i "s/\<JOB_TYPE\>/$var_job_type/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py
+sed -i "s/\<NUMB_MOLECULE\>/$var_molecule/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py
+sed -i "s/\<PARAMETER_VALUE\>/$var_parameter/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py
+sed -i "s/\<NUMB_PRESKIP\>/$var_preskip/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py
+python temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py
+echo -e "\n"
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
