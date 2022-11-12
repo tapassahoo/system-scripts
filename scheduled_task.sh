717df1144ec60e539242c9585c7ad6966d42bdca
Tapas Sahoo <tapascuchem@gmail.com>
2022-11-12 09:44:46 +0530

    Darwin: Updated on 12-11-2022

---
+++ scheduled_task.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+date 
+salloc --time=0-00:30 --ntasks=1 --account=rrg-pnroy_cpu
+#cd /home/tapas/MoRiBS-PIGS/examples/scripts/
+#cp generic_execution_moribs_driver_linear_molecule.py temp_execution_moribs_driver_linear_molecule_n5_beta01_nskip15000.py
+#sed -i 's/\<NUMB_MOLECULE\>/5/' temp_execution_moribs_driver_linear_molecule_n5_beta01_nskip15000.py 
+#sed -i 's/\<PARAMETER_VALUE\>/0.1/' temp_execution_moribs_driver_linear_molecule_n5_beta01_nskip15000.py
+#sed -i 's/\<NUMB_SKIP\>/15000/' temp_execution_moribs_driver_linear_molecule_n5_beta01_nskip15000.py
+#python temp_execution_moribs_driver_linear_molecule_n5_beta01_nskip15000.py
+pwd
+exit
+pwd
