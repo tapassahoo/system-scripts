90d058210620d5bc0daa519119e28b915567901c
Tapas Sahoo <tapascuchem@gmail.com>
2022-11-14 13:53:41 +0530

    Linux: Updated on 14-11-2022

---
+++ scheduled_cleaning.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo -e "\n"
+date 
+cd /home/tapas/MoRiBS-PIGS/examples/scripts/
+rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.[12]_nskip0.py
+rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.[12]_nskip10000.py
+rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.[12]_nskip15000.py
+date 
+echo -e "\n"
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
