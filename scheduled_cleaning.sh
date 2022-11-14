9bd80265a216aee5ae77fc8a0cdaba7af21fc94c
Tapas Sahoo <tapascuchem@gmail.com>
2022-11-14 15:22:28 +0530

    Linux: Updated on 14-11-2022

--- scheduled_cleaning.sh
+++ scheduled_cleaning.sh
@@ -3,9 +3,12 @@ echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo -e "\n"
 date 
 cd /home/tapas/MoRiBS-PIGS/examples/scripts/
-rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.[12]_nskip0.py
-rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.[12]_nskip10000.py
-rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.[12]_nskip15000.py
+rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.2_nskip0.py
+rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.2_nskip10000.py
+rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.2_nskip15000.py
+rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.1_nskip0.py
+rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.1_nskip10000.py
+rm temp_execution_moribs_driver_linear_molecule_n${numb_molecule}_beta0.1_nskip15000.py
 date 
 echo -e "\n"
 echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
