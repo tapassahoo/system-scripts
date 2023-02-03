f82c3094bb1a165112408250d01587f750a534c9
Tapas Sahoo <tapascuchem@gmail.com>
2023-02-03 16:00:02 +0530

    Linux: Updated on 03-02-2023

--- scheduled_cleaning.sh
+++ scheduled_cleaning.sh
@@ -4,12 +4,12 @@ echo -e "\n"
 date 
 var_molecule=$1
 cd /home/tapas/MoRiBS-PIGS/examples/scripts/
-rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.2_nskip0.py
-rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.2_nskip10000.py
-rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.2_nskip15000.py
-rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.1_nskip0.py
-rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.1_nskip10000.py
-rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.1_nskip15000.py
+rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.2_nskip0_analysis.py
+rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.2_nskip10000_analysis.py
+rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.2_nskip15000_analysis.py
+rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.1_nskip0_analysis.py
+rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.1_nskip10000_analysis.py
+rm temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta0.1_nskip15000_analysis.py
 date 
 echo -e "\n"
 echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
--- scheduled_task.sh
+++ scheduled_task.sh
@@ -12,12 +12,12 @@ echo "numb_molecule  = $var_molecule"
 echo "numb_parameter = $var_parameter"
 echo "numb_preskip   = $var_preskip"
 cd /home/tapas/MoRiBS-PIGS/examples/scripts/
-cp generic_execution_moribs_driver_linear_molecule.py temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py
-sed -i "s/\<JOB_TYPE\>/$var_job_type/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py
-sed -i "s/\<NUMB_MOLECULE\>/$var_molecule/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py
-sed -i "s/\<PARAMETER_VALUE\>/$var_parameter/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py
-sed -i "s/\<NUMB_PRESKIP\>/$var_preskip/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py
-python temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.py > ${HOME}/final-pigs-outputs-for-plotting/moribs_${var_job_type}_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.log &
+cp generic_execution_moribs_driver_linear_molecule.py temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}_analysis.py
+sed -i "s/\<JOB_TYPE\>/$var_job_type/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}_analysis.py
+sed -i "s/\<NUMB_MOLECULE\>/$var_molecule/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}_analysis.py
+sed -i "s/\<PARAMETER_VALUE\>/$var_parameter/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}_analysis.py
+sed -i "s/\<NUMB_PRESKIP\>/$var_preskip/" temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}_analysis.py
+python temp_execution_moribs_driver_linear_molecule_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}_analysis.py > ${HOME}/final-pigs-outputs-for-plotting/moribs_${var_job_type}_n${var_molecule}_beta${var_parameter}_nskip${var_preskip}.log &
 date 
 echo -e "\n"
 echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
