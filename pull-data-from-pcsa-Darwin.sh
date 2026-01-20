aa5a1416b95b126d7e84d89a762262d167f4aabb
“Tapas <tapascuchem@gmail.com>
2026-01-20 09:07:33 +0530

    Darwin: Updated on 20-01-2026

--- pull-data-from-pcsa-Darwin.sh
+++ pull-data-from-pcsa-Darwin.sh
@@ -9,7 +9,8 @@ set -xv  # Debug mode with verbose output
 # Sync output data from remote
 #-------------------------------
 REMOTE_OUTPUT="tapas:/home/tapas/academic-project/exact-diagonalization/pkg_monomer_rotor/output/"
-LOCAL_OUTPUT="${HOME}/academic-project/outputs/"
+#LOCAL_OUTPUT="${HOME}/academic-project/outputs/"
+LOCAL_OUTPUT="/Volumes/One Touch/pcsa-backup/outputs-of-exeact-diagonalization/"
 
 rsync -avz -e ssh \
   --exclude='**/*.*.tmp.*' \
